eee8d8fcf1e9bd4d91dd96a4521e300083177ea6
Retr0-t <88282611+Retr0-t@users.noreply.github.com>
2023-12-22 13:20:22 +0700

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,21 +1,20 @@
-
-**Quick Sort**
-"""
+"""**Quick Sort**"""
 
 import numpy as np
 from random import shuffle
 
-
 def partition(arr, left_index):
+    # Membagi array menjadi bagian kiri dan kanan
     left_part = arr[:left_index]
     right_part = arr[left_index:]
 
+    # Jika bagian kiri memiliki lebih dari satu elemen, lakukan pengurutan Quick Sort
     if len(left_part) > 1:
         quick_sort(left_part)
     
+    # Jika bagian kanan memiliki lebih dari satu elemen, lakukan pengurutan Quick Sort
     if len(right_part) > 1:
         quick_sort(right_part)
-
 
 def quick_sort(arr):
     last_index = len(arr) - 1
@@ -26,36 +25,33 @@ def quick_sort(arr):
     right_index = 0
 
     while left_index < right_index:
-        # Get item from left that is greater than pivot
+        # Cari elemen dari kiri yang lebih besar dari pivot
         left_index = last_index
         for index in range(last_index):
             if arr[index] > pivot:
                 left_index = index
                 break
 
-        # Get item from right that is smaller than pivot
+        # Cari elemen dari kanan yang lebih kecil dari pivot
         right_index = -1
         for index in range(last_index - 1, -1, -1):
             if arr[index] < pivot:
                 right_index = index
                 break
 
+        # Tukar elemen jika indeks kiri lebih besar dari indeks kanan
         if left_index > right_index:
             arr[left_index], arr[last_index] = arr[last_index], arr[left_index]
         else:
             arr[left_index], arr[right_index] = arr[right_index], arr[left_index]
 
+    # Rekursif bagi array berdasarkan indeks yang ditemukan
     partition(arr, left_index)
-    
 
-# Buat array berisi 0 sampai 999
+# Buat array berisi 0 sampai 999 dan acak isi array
 random_array = np.arange(10)
-
-# Acak isi array
 shuffle(random_array)
 
 print(random_array)
 
-quick_sort(random_array)
-
-##print(random_array)
+quick_sort(random_array)  # Panggil fungsi Quick Sort
