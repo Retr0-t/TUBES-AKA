ebd8bb42e6cf0311dd616295c2871daa6f0baf0d
Retr0-t <88282611+Retr0-t@users.noreply.github.com>
2023-12-27 13:27:09 +0700

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,60 +1,38 @@
-"""**Quick Sort**"""
+import random  # Tambahkan baris ini untuk mengimport modul random
 
-import numpy as np
-from random import shuffle
+def quick_sort(arr, low, high):
+    if low < high:
+        partition_index = partition(arr, low, high)
+        quick_sort(arr, low, partition_index)
+        quick_sort(arr, partition_index + 1, high)
 
-def partition(arr, left_index):
-    # Membagi array menjadi bagian kiri dan kanan
-    left_part = arr[:left_index]
-    right_part = arr[left_index:]
+def partition(arr, low, high):
+    pivot = arr[(low + high) // 2]
+    i = low - 1
+    j = high + 1
 
-    # Jika bagian kiri memiliki lebih dari satu elemen, lakukan pengurutan Quick Sort
-    if len(left_part) > 1:
-        quick_sort(left_part)
-    
-    # Jika bagian kanan memiliki lebih dari satu elemen, lakukan pengurutan Quick Sort
-    if len(right_part) > 1:
-        quick_sort(right_part)
+    while True:
+        i += 1
+        while arr[i] < pivot:
+            i += 1
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
+        j -= 1
+        while arr[j] > pivot:
+            j -= 1
 
-    pivot = arr[len(arr) // 2]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
+        if i >= j:
+            return j
 
-    return quick_sort(left) + middle + quick_sort(right)
+        arr[i], arr[j] = arr[j], arr[i]
 
-    while left_index < right_index:
-        # Cari elemen dari kiri yang lebih besar dari pivot
-        left_index = last_index
-        for index in range(last_index):
-            if arr[index] > pivot:
-                left_index = index
-                break
+# Buat array berisi 0 sampai 9 dan acak isi array
+random_array = list(range(10))
+random.shuffle(random_array)  # Menggunakan fungsi shuffle dari modul random
 
-        # Cari elemen dari kanan yang lebih kecil dari pivot
-        right_index = -1
-        for index in range(last_index - 1, -1, -1):
-            if arr[index] < pivot:
-                right_index = index
-                break
-
-        # Tukar elemen jika indeks kiri lebih besar dari indeks kanan
-        if left_index > right_index:
-            arr[left_index], arr[last_index] = arr[last_index], arr[left_index]
-        else:
-            arr[left_index], arr[right_index] = arr[right_index], arr[left_index]
-
-    # Rekursif bagi array berdasarkan indeks yang ditemukan
-    partition(arr, left_index)
-
-# Buat array berisi 0 sampai 999 dan acak isi array
-random_array = np.arange(10)
-shuffle(random_array)
-
+print("Sebelum pengurutan:")
 print(random_array)
 
-quick_sort(random_array)  # Panggil fungsi Quick Sort
+quick_sort(random_array, 0, len(random_array) - 1)  # Panggil fungsi Quick Sort
+
+print("Setelah pengurutan:")
+print(random_array)
